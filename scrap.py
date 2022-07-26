1098b360d570a3165ec7fb60ed7061f0ba3657c1
Tom <tomasechavarriab@gmail.com>
2022-07-26 14:19:03 -0500

    working

--- scrap.py
+++ scrap.py
@@ -10,9 +10,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-R_EMAIL_ADDRESS = os.environ['R_EMAIL_ADDRESS']
-S_EMAIL_PWSD = os.environ['S_EMAIL_PWSD']
-S_EMAIL_ADDRESS = os.environ['S_EMAIL_ADDRESS']
+R_EMAIL_ADDRESS = 'felipe.gutierreze@hotmail.com'#os.environ['R_EMAIL_ADDRESS']
+S_EMAIL_PWSD = 'tfhaxuyryjmpvcec'#os.environ['S_EMAIL_PWSD']
+
+S_EMAIL_ADDRESS = 'Datatroopermailservice@gmail.com'#os.environ['S_EMAIL_ADDRESS']
 
 
 personal_email_info = {'email': S_EMAIL_ADDRESS, 'password': S_EMAIL_PWSD}
@@ -32,19 +33,19 @@ while 1:
         print('respuesta: ',response)
         soup = BeautifulSoup(response.content, 'html.parser')
         results = soup.find_all('a', {'class': 'a-size-base a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal'})
-        #print(results)
+        print(results)
         df = pd.DataFrame(columns=['Fecha', 'Busqueda', 'Producto', 'Precio', 'Descuento', 'Link'])
 
         for res in results:
             try:
-                res_disc = res.find('span', {'class': 'a-size-extra-large s-color-discount s-light-weight-text'}).text
+                res_disc = res.find('span', {'class': 'a-size-extra-large s-color-discount puis-light-weight-text'}).text
                 link = f'https://www.amazon.com/-/es{res["href"]}'
                 price = res.find('span', {'class': 'a-offscreen'}).text
                 name = res.parent.parent.parent.find('span', {'class': 'a-size-base-plus a-color-base a-text-normal'}).text
 
                 df = df.append({'Fecha': datetime.datetime.now(), 'Busqueda': query_product, 'Producto': name, 'Precio': price, 'Descuento': res_disc, 'Link': link}, ignore_index=True)
             except Exception as e:
-                print(e)
+                #print(e)
                 pass
 
         df['Descuento'] = df['Descuento'].str.replace('%', '').str.replace('-', '').astype(float)
@@ -68,7 +69,7 @@ while 1:
         print('Resultados guardados en Precios amazon.xlsx')
 
         print('Esperando para volver a buscar...')
-        time.sleep(60)
+        time.sleep(60*60)
     except Exception as e:
         
         print('Error: ', e)
